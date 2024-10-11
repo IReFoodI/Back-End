7beeb752ed60a0746742f8ddb43a3c962a405631
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-11 20:01:52 -0300

    refactor: AddressService

--- refoods/src/main/java/com/projeto/ReFood/service/AddressService.java
+++ refoods/src/main/java/com/projeto/ReFood/service/AddressService.java
@@ -3,7 +3,6 @@ package com.projeto.ReFood.service;
 import com.projeto.ReFood.repository.AddressRepository;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
