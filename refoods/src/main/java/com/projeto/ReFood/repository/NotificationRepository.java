6b743a2e26a22d34e9210ae7838b6b9f08e56458
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-11 15:46:35 -0300

    feat: primeira revisao de repositories

--- refoods/src/main/java/com/projeto/ReFood/repository/NotificationRepository.java
+++ refoods/src/main/java/com/projeto/ReFood/repository/NotificationRepository.java
@@ -2,8 +2,6 @@ package com.projeto.ReFood.repository;
 
 import com.projeto.ReFood.model.Notification;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
