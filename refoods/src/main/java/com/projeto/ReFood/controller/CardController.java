23ee28809cb9a6d334ee1d489e59e584e2ff2d04
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-12 06:49:25 -0300

    refactor: card controller

--- refoods/src/main/java/com/projeto/ReFood/controller/CardController.java
+++ refoods/src/main/java/com/projeto/ReFood/controller/CardController.java
@@ -1,48 +1,59 @@
 package com.projeto.ReFood.controller;
 
 import com.projeto.ReFood.service.CardService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.projeto.ReFood.dto.CardDTO;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.projeto.ReFood.dto.CardDTO;
+import com.projeto.ReFood.exception.NotFoundException;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cards")
+@RequestMapping("/api/card")
 public class CardController {
-    
+
     @Autowired
     private CardService cardService;
-    
+
     @GetMapping
-    public List<CardDTO> getAllCards() {
-        return cardService.getAllCards();
+    public ResponseEntity<List<CardDTO>> listAllCards() {
+        List<CardDTO> cards = cardService.getAllCards();
+        return ResponseEntity.ok(cards);
     }
-    
-    @GetMapping("/{id_card}")
-    public ResponseEntity<CardDTO> getCardById(@PathVariable int id_card) {
-        CardDTO cardDTO = cardService.getCardById(id_card);
-        
-        return cardDTO != null ? ResponseEntity.ok(cardDTO) : ResponseEntity.notFound().build();
+
+    @GetMapping("/{cardId}")
+    public ResponseEntity<CardDTO> getCardById(@PathVariable Long cardId) throws NotFoundException {
+        CardDTO cardDTO = cardService.getCardById(cardId);
+        return ResponseEntity.ok(cardDTO);
     }
-    
+
     @PostMapping
-    public CardDTO createCard(@RequestBody CardDTO cardDTO) {
-        return cardService.createCard(cardDTO);
+    public ResponseEntity<CardDTO> createCard(@Valid @RequestBody CardDTO cardDTO) {
+        CardDTO createdCard = cardService.createCard(cardDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{cardId}")
+                .buildAndExpand(createdCard.cardId())
+                .toUri();
+        return ResponseEntity.created(location).body(createdCard);
     }
-    
-    @PutMapping("/{id_card}")
-    public ResponseEntity<CardDTO> updateCard(@PathVariable int id_card, @RequestBody CardDTO cardDTO) {
-        CardDTO updateCard = cardService.updateCard(id_card, cardDTO);
-        
-        return updateCard != null ? ResponseEntity.ok(updateCard) : ResponseEntity.notFound().build();
+
+    @PutMapping("/{cardId}")
+    public ResponseEntity<CardDTO> updateCard(@PathVariable Long cardId, @Valid @RequestBody CardDTO cardDTO)
+            throws NotFoundException {
+        CardDTO updatedCard = cardService.updateCard(cardId, cardDTO);
+        return ResponseEntity.ok(updatedCard);
     }
-    
-    @DeleteMapping("/{id_card}")
-    public ResponseEntity<Void> deleteCard(@PathVariable int id_card) {
-        cardService.deleteCard(id_card);
-        
+
+    @DeleteMapping("/{cardId}")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long cardId) throws NotFoundException {
+        cardService.deleteCard(cardId);
         return ResponseEntity.noContent().build();
     }
 }
