18e581e614142d1303f017ac2e22f50c9346b063
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-12 16:34:24 -0300

    feat: add dependencia validation

--- refoods/src/main/java/com/projeto/ReFood/security/SecurityConfig.java
+++ refoods/src/main/java/com/projeto/ReFood/security/SecurityConfig.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,64 +18,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.projeto.ReFood.service.CustomUserDetailsService;
 
 @Configuration
-@EnableWebSecurity // 3333333333333333
+@EnableWebSecurity
 public class SecurityConfig {
-  private final JwtAuthenticationFilter jwtAuthFilter;
-  private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
-  public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter,
-      CustomUserDetailsService customUserDetailsService) {
-    this.jwtAuthFilter = jwtAuthFilter;
-    this.customUserDetailsService = customUserDetailsService;
-  }
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter,
+                          CustomUserDetailsService customUserDetailsService) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-    return config.getAuthenticationManager();
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = 
+            http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public AuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(customUserDetailsService);
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
-  }
+    @Bean
+    @Profile("test")
+    @Order(1)
+    public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(PathRequest.toH2Console())
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
 
-  @Bean
-  @Profile("test")
-  @Order(1)
-  public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
-    http.securityMatcher(PathRequest.toH2Console())
-        .csrf(csrf -> csrf.disable())
-        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                // .requestMatchers("/auth/**").permitAll() // login
+                // .requestMatchers("/h2-console/**").permitAll() // h2 database
+                // .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // swagger
+                .requestMatchers(HttpMethod.GET, "/user").hasRole("USER") // apenas uma rota para testar autenticação
+                .requestMatchers(HttpMethod.GET, "/restaurant").hasRole("RESTAURANT") // apenas uma rota para testar autenticação
+                // .requestMatchers(HttpMethod.POST, "/api/user").permitAll() // create user
+                // .requestMatchers(HttpMethod.POST, "/api/restaurant").permitAll() // create restaurant
+                .anyRequest().permitAll() // permitindo todas as requisições temporariamente para teste
+                //.anyRequest().authenticated() // Bloqueia todas as outras requisições
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-    http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/**").permitAll() // login
-            .requestMatchers("/h2-console/**").permitAll() // h2 database
-            .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // swagger
-            .requestMatchers(HttpMethod.GET, "/user").hasRole("USER") 
-            .requestMatchers(HttpMethod.GET, "/restaurant").hasRole("RESTAURANT") // apenas uma rota para testar autenticação
-            .requestMatchers(HttpMethod.POST, "/api/user").permitAll() // create user
-            .requestMatchers(HttpMethod.POST, "/api/restaurant").permitAll() // create restaurant
-            .anyRequest().permitAll() // To permitindo todas as requisições temporariamente para teste
-            // .anyRequest().authenticated() // Bloqueia todas as outras requisições
-        )
-        .authenticationProvider(authenticationProvider())
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-    return http.build();
-  }
-
+        return http.build();
+    }
 }
+
