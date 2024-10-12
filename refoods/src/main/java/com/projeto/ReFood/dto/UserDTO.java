e17ed1ca28a5d760197aba71a26e484a75ecb47a
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-12 16:53:31 -0300

    fix: add annotation @EnableMethodSecurity() e @PreAuthorize()

--- refoods/src/main/java/com/projeto/ReFood/dto/UserDTO.java
+++ refoods/src/main/java/com/projeto/ReFood/dto/UserDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record UserDTO(
