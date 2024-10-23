d6c5e982338916db3174503a79622efc85de641c
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-10-23 07:16:15 -0400

    refactor: :art: Removidos imports não usados

--- refoods/src/main/java/com/projeto/ReFood/model/Address.java
+++ refoods/src/main/java/com/projeto/ReFood/model/Address.java
@@ -3,7 +3,6 @@ package com.projeto.ReFood.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
