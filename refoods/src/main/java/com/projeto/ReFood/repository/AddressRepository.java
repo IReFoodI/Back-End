98b9114a0bb4c38aedcce491e9fd0ee52c0622d2
Samilis Brito <samilisbritto@gmail.com>
2024-10-23 22:07:25 -0300

    🗃️ feat: added custom queries in AddressRepository for user-specific address retrieval
    - Implemented a query to find an address by its ID and the associated user's ID:
      - @Query("SELECT a FROM Address a WHERE a.id = :addressId AND a.user.id = :userId")
      - Method: Optional<Address> findByIdAndUserId(Long addressId, Long userId);
      - This ensures the query returns the correct address belonging to the specific user.
    
    - Introduced another query to fetch the default address for a user:
      - @Query("SELECT a FROM Address a WHERE a.user.id = :userId AND a.isStandard = true")
      - Method: Optional<Address> findByUserIdAndIsStandardTrue(Long userId);
      - This query retrieves the default address marked as `isStandard = true`.

--- refoods/src/main/java/com/projeto/ReFood/repository/AddressRepository.java
+++ refoods/src/main/java/com/projeto/ReFood/repository/AddressRepository.java
@@ -6,8 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
     @Query("SELECT a FROM Address a WHERE a.user.id = :userId")
     List<Address> findAddressesByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT a FROM Address a WHERE a.id = :addressId AND a.user.id = :userId")
+    Optional<Address> findByIdAndUserId(@Param("addressId") Long addressId, @Param("userId") Long userId);
+
+    @Query("SELECT a FROM Address a WHERE a.user.id = :userId AND a.isStandard = true")
+    Optional<Address> findByUserIdAndIsStandardTrue(@Param("userId") Long userId);
 }
