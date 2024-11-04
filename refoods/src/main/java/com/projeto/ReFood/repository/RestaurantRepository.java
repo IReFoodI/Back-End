1ce6df71033db898d2b152c8fa630e209b7c6edc
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-11-04 20:08:09 -0300

    Feat: Add find restaurant email by id query

--- refoods/src/main/java/com/projeto/ReFood/repository/RestaurantRepository.java
+++ refoods/src/main/java/com/projeto/ReFood/repository/RestaurantRepository.java
@@ -3,6 +3,9 @@ package com.projeto.ReFood.repository;
 import com.projeto.ReFood.model.Restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
@@ -10,4 +13,7 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
   boolean existsByEmail(String email);
   boolean existsByCnpj(String cnpj);
   Optional<Restaurant> findByCnpj(String cnpj);
+
+  @Query("SELECT r.email FROM Restaurant r WHERE r.id = :id")
+  Optional<String> findEmailById(@Param("id") Long id);
 }
