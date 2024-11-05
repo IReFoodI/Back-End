ee95c9ddd145b7a7e5bc5fdf9b2f01a9e15f77c0
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-11-04 23:21:18 -0300

    feat: rota auth/user/info

--- refoods/src/main/java/com/projeto/ReFood/controller/AuthController.java
+++ refoods/src/main/java/com/projeto/ReFood/controller/AuthController.java
@@ -2,11 +2,13 @@ package com.projeto.ReFood.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.projeto.ReFood.dto.GoogleDTO;
 import com.projeto.ReFood.dto.LoginRequest;
 import com.projeto.ReFood.dto.LoginResponse;
+import com.projeto.ReFood.model.UserInfo;
 import com.projeto.ReFood.service.AuthService;
 
 @RestController
@@ -20,11 +22,18 @@ public class AuthController {
     this.authService = authService;
   }
 
+  @GetMapping("/user/info")
+  @PreAuthorize("hasAnyRole('USER', 'RESTAURANT')")
+  public ResponseEntity<UserInfo> getUserInfo() {
+    UserInfo userInfo = authService.getCurrentUserInfo();
+    return ResponseEntity.ok(userInfo);
+  }
+
   @PostMapping("user/login")
   public ResponseEntity<LoginResponse> createUserAuthenticationToken(@RequestBody LoginRequest loginRequest) {
     return authService.authenticateUser(loginRequest.email(), loginRequest.password());
   }
-  
+
   @PostMapping("restaurant/login")
   public ResponseEntity<LoginResponse> createRestaurantAuthenticationToken(@RequestBody LoginRequest loginRequest) {
     return authService.authenticateRestaurant(loginRequest.email(), loginRequest.password());
